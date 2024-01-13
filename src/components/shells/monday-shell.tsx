5c531b30afb615236b02fb8d9458aa7a23fe1e03
yuda <zerone777@gmail.com>
2024-01-13 17:42:48 +0700

    datatable monday

--- src/components/shells/monday-shell.tsx
+++ src/components/shells/monday-shell.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { type ColumnDef } from "unstyled-table"
 import { formatDate } from "@/lib/utils"
 import { Checkbox } from "@/components/ui/checkbox"
-import { DataTable } from "@/components/data-table/data-table-customer"
+import { DataTable } from "@/components/data-table/data-table-monday"
 import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header"
 
 
